27719918c9dcd388e221054a26fcfbe4b7d48692
Ahmad Tantowi <ahmadtantowi@outlook.com>
2025-12-09 16:20:25 +0700

    Add cache middleware

---
+++ server/middleware/cache.go
@@ -0,0 +1,17 @@
+package middleware
+
+import (
+	"dpb-online/version"
+	"net/http"
+)
+
+func CacheMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if version.Value == "dev" {
+			w.Header().Set("Cache-Control", "no-cache")
+		} else {
+			w.Header().Set("Cache-Control", "public, max-age=31536000")
+		}
+		next.ServeHTTP(w, r)
+	})
+}
